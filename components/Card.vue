7579e50f9673b2bccd0669a78591fa0cd3839638
CODE-BR3AKER <zarktizakaria@gmail.com>
2021-10-22 16:18:22 -0400

    removed sass

--- components/Card.vue
+++ components/Card.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="Padding w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
     <nuxt-link
-      class="block h-full shadow-lg rounded-lg overflow-hidden mt-10 ml-2 mr-2 sm:mt-0 sm:mr-0 sm:ml-0 bg-gray-DEFAULT dark:bg-blue-DEFAULT"
+      class="
+        block
+        h-full
+        shadow-lg
+        rounded-lg
+        overflow-hidden
+        mt-10
+        ml-2
+        mr-2
+        sm:mt-0 sm:mr-0 sm:ml-0
+        bg-gray-DEFAULT
+        dark:bg-blue-DEFAULT
+      "
       :to="where"
     >
       <slot name="picture"> </slot>
       <div class="p-6">
         <h1
-          class="title-font text-lg font-bold text-gray-900 mb-2 dark:text-gray-DEFAULT"
+          class="
+            title-font
+            text-lg
+            font-bold
+            text-gray-900
+            mb-2
+            dark:text-gray-DEFAULT
+          "
         >
           {{ title }}
         </h1>
@@ -35,10 +54,12 @@ export default {
   props: ["title", "population", "region", "capital", "where"],
 };
 </script>
-<style lang="scss">
+<style>
 .Padding {
   padding: 2.5rem;
-  @media screen and (max-width: "640px") {
+}
+@media screen and (max-width: "640px") {
+  .Padding {
     padding: 2rem;
     margin-bottom: 1em;
   }
